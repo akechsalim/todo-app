be377ef4f231ae38284543c5ecd8d59a19fd07ad
akechsalim <akechsalimwork@gmail.com>
2023-09-27 10:51:53 +0300

    modified App.js,App.css and added new jsx file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import { Component } from 'react';
+import './App.css'
+import FirstComponent from './components/learning-examples/FirstComponent'
 
 function App() {
   return (
     <div className="App">
-      My Todo Application
+      <FirstComponent/>
+      <SecondComponent/>
+      <ThirdComponent/>
+      <FourthComponent/>
     </div>
   );
+}
+
+function SecondComponent(){
+  return(
+    <div className='SecondComponent'>SecondComponent</div>
+  )
+}
+class ThirdComponent extends Component{
+  render(){
+    return(
+      <div className='ThirdComponent'>ThirdComponent</div>
+    )
+  }
+}
+class FourthComponent extends Component{
+  render(){
+    return(
+    <div className='FourthComponent'>FourthComponent</div>
+    )
+  }
 }
 
 export default App;
