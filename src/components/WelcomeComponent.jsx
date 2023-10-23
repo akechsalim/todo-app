4a28916d1517e26b5abd41f236879c11a89da057
akechsalim <akechsalimwork@gmail.com>
2023-10-23 15:03:26 +0300

    changes

--- src/components/WelcomeComponent.jsx
+++ src/components/WelcomeComponent.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { retrieveHelloWorldPathVariable } from './API/HelloWorldAPIService'
 function WelcomeComponent() {
 
     const { username } = useParams()
-    console.log(username)
+
+    const [message, setMessage] = useState(null)
+
+    function callHelloWorldRestApi() {
+
+        console.log('called')
+
+        retrieveHelloWorldPathVariable('Salim')
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log('cleanup'))
+
+
+    }
+    function successfulResponse(response) {
+        console.log(response)
+        // setMessage(response.data)
+        setMessage(response.data.message)
+    }
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div className="Welcome">
@@ -10,6 +33,11 @@ function WelcomeComponent() {
             <div>
                 Manage your todos. <Link to='/todos'>Go here</Link>
             </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    call Hello World</button>
+            </div>
+            <div className='text-info'>{message}</div>
         </div>
     )
 }
